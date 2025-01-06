4a983640636825d845d61cea2fd940ce9bfee9e8
Sven Jacob <jsve@gmx.de>
2025-01-06 15:37:57 +0100

    update

--- ChangeDetection/utilsCPD.py
+++ ChangeDetection/utilsCPD.py
@@ -11,6 +11,47 @@ def mom_estimates(X):
     return alpha_hat, beta_hat
 
 
+
+def estimate_parameter_CI(X,p):
+    alpha_hat, beta_hat = mom_estimates(X)
+    n= len(X)
+    var_alpha_hat = (6* alpha_hat**2) / n  #+ beta_hat**4*var_X
+    var_beta_hat = beta_hat**2/(n*alpha_hat) + beta_hat**6/alpha_hat**2*(2*alpha_hat**2)/beta_hat**4/n
+
+    # Confidence level (e.g., 95%)
+    confidence_level = p
+    z_alpha_2 = norm.ppf(1 - (1 - confidence_level) / 2)  # Critical value for normal distribution
+
+    # Confidence intervals
+    alpha_CI = (alpha_hat - z_alpha_2 * np.sqrt(var_alpha_hat), alpha_hat + z_alpha_2 * np.sqrt(var_alpha_hat))
+    beta_CI = (beta_hat - z_alpha_2 * np.sqrt(var_beta_hat), beta_hat + z_alpha_2 * np.sqrt(var_beta_hat))
+
+    return alpha_CI,beta_CI
+
+def bootstrap_mom_estimates(X, n_bootstrap=1000, random_seed=None):
+    
+    if random_seed is not None:
+        np.random.seed(random_seed)
+
+    n = len(X)
+    alpha_bootstrap = np.empty(n_bootstrap)
+    beta_bootstrap = np.empty(n_bootstrap)
+
+    for i in range(n_bootstrap):
+        # Resample with replacement
+        bootstrap_sample = np.random.choice(X, size=n, replace=True)
+        # Compute method of moments estimates for the bootstrap sample
+        alpha_hat, beta_hat = mom_estimates(bootstrap_sample)
+        alpha_bootstrap[i] = alpha_hat
+        beta_bootstrap[i] = beta_hat
+    
+    alpha_ci = np.percentile(alpha_bootstrap, [2.5, 97.5])  # 95% CI for alpha
+    beta_ci = np.percentile(beta_bootstrap, [2.5, 97.5])
+
+    return alpha_ci[0], beta_ci[1]
+
+
+
 class TimeseriesDataset(torch.utils.data.Dataset):   
     def __init__(self, X, seq_len=10,split=0.5):
         self.X = X
